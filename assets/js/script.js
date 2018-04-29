5f70c9943e0f9de191ae1a8052be74306dbc7010
vsv <vsv21.cv@gmail.com>
2018-04-29 20:11:25 +0300

    fix article page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,7 @@
 jQuery(function ($) {
 	$('.articles_desc, .articles_container, .article_details.full, .header, .article-page-title, .footer, .article_details.text, .pages_selector, .products_table_container').addClass('container');
 	$('.articles_container .article_details.short').each(function () {
-
-		
-
-		var descText = $(this).find('.desc_short_news').text().split('\n');
+                var descText = $(this).find('.desc_short_news').text().split('\n');
 		if (descText.length > 0) {
 			var descHtml = '';
 			for (var i = 0; i < descText.length; i++) {
@@ -18,26 +15,12 @@ jQuery(function ($) {
 		}
 
 	});
-	$('.article_details.full .full_desc_news').prepend($('.article_details.full h1'));
-	$('.article_details.full .full_desc_news').append($('.article_details.full .date'));
 	$('.article_details.full .full_desc_news').append($('.article-share'));
 	$('.article_details.full .full_desc_news').append($('.buy-pic-form'));
-	$('.top_menu').before($('a.logo'));
-	$('.top_menu').prepend('<div class="slicknav_menu"><a href="#" class="slicknav_btn slicknav_collapsed"><span class="slicknav_icon slicknav_no-text"><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span></span></a></div>');
-
-	var str = $('.who_make a:first-child').text(),
-		newStr = '';
-	if (str.indexOf("totalcan") != -1) {
-		newStr = str.substring(0, str.length - 16);
-		$('.who_make a:first-child').addClass('totalcan').text(newStr);
-	} else if (str.indexOf("ARTWEB") != -1) {
-		newStr = str.substring(0, str.length - 11);
-    	$('.who_make a:first-child').addClass('artweb').text(newStr);
-	}
 
 	$('.buy-pic-form .input_line:not(:last-child)').wrapAll('<div class="input_line-wrp" />');
 	$('.buy-pic-form input[type="button"]').after('<a class="no-send" />');
-	$('.input_line.button .inp_box_right').addClass('no-send-wrp')
+	$('.input_line.button .inp_box_right').addClass('no-send-wrp');
 	$('.no-send').on('click', function (e) {
 		e.preventDefault();
 		$('.input_line-wrp').slideDown();
